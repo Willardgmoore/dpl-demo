b8acd0c491f19014992a0c2498c2370a871ebde4
bondparkerbond <parkerbond@gmail.com>
2015-08-20 16:21:22 -0600

    Inital Commit

---
+++ day4menu.rb
@@ -0,0 +1,6 @@
+#Parker and Willard Cafetria Code!
+@menu = {
+  entree = ['Fried Chicken', 'Roast Turkey', 'Meat Loaf', 'Chicken Pot Pie']
+  side = ['Baked Potatoe', 'Green Salad', 'Cup of Fruit', 'Mac and Cheese']
+}
+puts "#{@menu[entree]}"
